4e404d2a7200bbbf8bca2d613aab5b225cf24a71
prosfp <110301233+prosfp@users.noreply.github.com>
2023-10-24 14:39:23 +0200

    exercici1 update

--- exercises/Exercici1.js
+++ exercises/Exercici1.js
@@ -2,4 +2,19 @@
 
 const platsPreferits = ['pa amb tomàquet', 'pizza', 'hamburguesa', 'gelat'];
 
-// Una funció que mostri per consola tot aquesta llista
+// Una funció que mostri per consola tot aquesta llista amb
+// el títol "El meus plats preferits són: plat1, plat2, plat3...".
+
+function mostrarPlatsPreferits() {
+  console.log('Els meus plats preferits són: ' + platsPreferits.join(', '));
+}
+
+// O amb un for..of:
+
+function mostrarPlatsPreferits2() {
+  let missatge = 'Els meus plats preferits són: ';
+  for (plat of platsPreferits) {
+    missatge += plat + ', ';
+  }
+  console.log(missatge);
+}
